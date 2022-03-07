d5680b0338de9d849d9465dd7f2b6a75800e2012
Nick Witten <nwitt12@gmail.com>
2022-03-07 15:02:04 -0500

    Uncomment main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-//#include "mbed.h"
-//
-//Serial pc(USBTX, USBRX);
-//int main() {
-//    pc.printf("Hello World!\r\n");
-//}
+#include "mbed.h"
+
+Serial pc(USBTX, USBRX);
+int main() {
+    pc.printf("Hello World!\r\n");
+}
